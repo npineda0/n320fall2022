7f3b029f004bac6703ac1563f990c5e2862fa055
npineda0 <97705502+npineda0@users.noreply.github.com>
2022-10-20 20:31:58 -0400

    edits

--- projectOne/js/app.js
+++ projectOne/js/app.js
@@ -1,45 +1,39 @@
+
 //board game
 class ConnectFour {
 
     //class properties
-    playerOneColor = "#c7364e";     //orange color
+    playerOneColor = "#d1241b";     //red color
     playerTwoColor = "#f5e640";     //yellow color
-    board = document.getElementsByClassName("gridBoard");   //connect4 board
-    gridCirc = document.querySelectorAll("gridCircle");
+    board = document.getElementById("gridBoard");   //connect4 board
 
     constructor() {
-        //for loop for the whole grid
-        for(var i = 0; i < 42; i++) {
-            //start at bottom row
-
-
-            //tokens can only be stacked, not placed randomly 
-
-
-            //mouse event for clicking grid circle to change color
-            this.gridCirc.addEventListener("click", (event) => {
-                event.target.style.backgroundColor = this.playerOneColor;
-            })
-            //if statement to alternate color between each click
-
-
-            //if statement to check if playerOne or playerTwo color connects vertically
-                //else horizontal 
-                    //else diagonaly
-
-            //if they connect4 game ends
+        for( var x=0; x < 680; x+= 7){
+            for( var y=0; y < 600; y+= 6 ){
+                let gridCirc = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+                gridCirc.classList.add("gridCircle");
+                gridCirc.setAttributeNS(null,'x',x );
+                gridCirc.setAttributeNS(null,'y',y );
+                gridCirc.setAttributeNS(null,'r', 50);
+                this.board.appendChild(gridCirc);
+            }
         }
-    }
-}
+       /*
+        //make grid circles
+        for(var i = 0; i < 42; i++) {
+            //create circle
+            let gridCirc = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            //circle class
+            gridCirc.classList.add("gridCircle");
+            //places circles on x & y axis,
+            gridCirc.setAttribute("cx", Math.random()* 700);
+            gridCirc.setAttribute("cy", Math.random()* 600);
 
-class Player {
-    constructor(color) {
-        this.color=color;
+            //add white circle to the screen
+            this.board.appendChild(gridCirc);
+        }
+        */
     }
 }
+let game = new ConnectFour;
 
-class Player2 extends Player {
-    constructor() {
-        super(color, "#f5e640");
-    }
-}
